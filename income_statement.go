444bb488b1c8dd930d9662bd617592142df51180
John Wang <johncwang@gmail.com>
2024-07-15 00:51:34 -0700

    feat: `financialmodelingprep`: add `IncomeStatementParse()`, `IncomeStatements.RevenueTimeSeries()`, `IncomeStatements.Periods()`

--- income_statement.go
+++ income_statement.go
@@ -3,6 +3,7 @@ package financialmodelingprep
 import (
 	"encoding/json"
 	"errors"
+	"io"
 	"os"
 	"strconv"
 	"time"
@@ -10,13 +11,11 @@ import (
 	"github.com/grokify/gocharts/v2/charts/wchart/sts2wchart"
 	"github.com/grokify/gocharts/v2/data/timeseries"
 	"github.com/grokify/mogo/time/timeutil"
+	"github.com/grokify/mogo/type/slicesutil"
 	chart "github.com/wcharczuk/go-chart/v2"
 )
 
-const (
-	APIEndpoint        = "https://financialmodelingprep.com"
-	IncomeStatementURL = "api/v3/income-statement"
-)
+const PeriodFY = "FY"
 
 // https://financialmodelingprep.com/api/v3/income-statement/AAPL?limit=120&apikey=mykey
 
@@ -28,18 +27,67 @@ type IncomeStatementOpts struct {
 
 type IncomeStatements []IncomeStatement
 
-func ParseIncomeStatementsFile(filename string) (IncomeStatements, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
+func IncomeStatementsReadFile(filename string) (IncomeStatements, error) {
+	if f, err := os.Open(filename); err != nil {
 		return IncomeStatements{}, err
+	} else if res, err := IncomeStatementParse(f); err != nil {
+		return IncomeStatements{}, err
+	} else {
+		return res, f.Close()
 	}
-	return ParseIncomeStatements(data)
 }
 
-func ParseIncomeStatements(data []byte) (IncomeStatements, error) {
+func IncomeStatementParse(r io.Reader) (IncomeStatements, error) {
+	if b, err := io.ReadAll(r); err != nil {
+		return IncomeStatements{}, err
+	} else {
+		return IncomeStatementsParseBytes(b)
+	}
+}
+
+func IncomeStatementsParseBytes(data []byte) (IncomeStatements, error) {
 	incs := IncomeStatements{}
 	err := json.Unmarshal(data, &incs)
 	return incs, err
+}
+
+/*
+// RevenueTimeSeries returns a time series of revnue. The periods of the income statements
+// must be the same.
+func (iss IncomeStatements) RevenueTimeSeries() (*timeseries.TimeSeries, error) {
+	periods := iss.Periods(true)
+	if len(periods) > 1 {
+		return nil, errors.New("too many time periods")
+	} else if len(periods) == 0 {
+		return nil, errors.New("no time periods")
+	}
+	ts := timeseries.NewTimeSeries("income statements")
+	ts.IsFloat = true
+	if periods[0] == PeriodFY {
+		ts.Interval = timeutil.IntervalYear
+	} else {
+		return nil, fmt.Errorf("interval not supported (%s)", periods[0])
+	}
+	for _, is := range iss {
+		dt, err := time.Parse(timeutil.RFC3339FullDate, is.Date)
+		if err != nil {
+			return nil, err
+		}
+		ts.AddFloat64(dt, is.Revenue)
+	}
+	return &ts, nil
+}
+*/
+
+func (iss IncomeStatements) Periods(dedupe bool) []string {
+	var periods []string
+	for _, is := range iss {
+		periods = append(periods, is.Period)
+	}
+	if dedupe {
+		return slicesutil.Dedupe(periods)
+	}
+	return periods
 }
 
 func (iss IncomeStatements) ChartAnnual() (chart.Chart, error) {
@@ -136,9 +184,11 @@ func Ordered() []string {
 }
 
 type IncomeStatement struct {
-	Symbol                           string  `json:"symbol"`
-	FillingDate                      string  `json:"fillingDate"`
 	CalendarYear                     string  `json:"calendarYear"`
+	Date                             string  `json:"date"`
+	FillingDate                      string  `json:"fillingDate"`
+	Period                           string  `json:"period'`
+	Symbol                           string  `json:"symbol"`
 	Revenue                          float64 `json:"revenue"`
 	CostOfRevenue                    float64 `json:"costOfRevenue"`
 	GrossProfit                      float64 `json:"grossProfit"`
@@ -165,3 +215,46 @@ type IncomeStatement struct {
 func (is *IncomeStatement) CalendarYearInt() (int, error) {
 	return strconv.Atoi(is.CalendarYear)
 }
+
+/*
+  {
+    "date": "2021-12-31",
+    "symbol": "JPM",
+    "reportedCurrency": "USD",
+    "cik": "0000019617",
+    "fillingDate": "2022-02-22",
+    "acceptedDate": "2022-02-22 16:56:31",
+    "calendarYear": "2021",
+    "period": "FY",
+    "revenue": 130898000000,
+    "costOfRevenue": 0,
+    "grossProfit": 130898000000,
+    "grossProfitRatio": 1,
+    "researchAndDevelopmentExpenses": 0,
+    "generalAndAdministrativeExpenses": 19755000000,
+    "sellingAndMarketingExpenses": 3036000000,
+    "sellingGeneralAndAdministrativeExpenses": 65874000000,
+    "otherExpenses": 0,
+    "operatingExpenses": 71336000000,
+    "costAndExpenses": 71336000000,
+    "interestIncome": 57864000000,
+    "interestExpense": 5553000000,
+    "depreciationAndAmortization": -59988000000,
+    "ebitda": -2257000000,
+    "ebitdaratio": -0.017242433,
+    "operatingIncome": 59988000000,
+    "operatingIncomeRatio": 0.4582804932,
+    "totalOtherIncomeExpensesNet": -426000000,
+    "incomeBeforeTax": 59562000000,
+    "incomeBeforeTaxRatio": 0.4550260508,
+    "incomeTaxExpense": 11228000000,
+    "netIncome": 46503000000,
+    "netIncomeRatio": 0.3552613485,
+    "eps": 15.39,
+    "epsdiluted": 15.36,
+    "weightedAverageShsOut": 3027539062,
+    "weightedAverageShsOutDil": 3026600000,
+    "link": "https://www.sec.gov/Archives/edgar/data/19617/000001961722000272/0000019617-22-000272-index.htm",
+    "finalLink": "https://www.sec.gov/Archives/edgar/data/19617/000001961722000272/jpm-20211231.htm"
+  },
+*/
