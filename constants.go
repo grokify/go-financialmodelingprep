70472f5c503e6b0e8cbbdcfc49c38bafaf2335ca
John Wang <johncwang@gmail.com>
2024-07-15 00:58:10 -0700

    feat: `financialmodelingprep`: add constants

---
+++ constants.go
@@ -0,0 +1,13 @@
+package financialmodelingprep
+
+const (
+	ServerURL                    = "https://financialmodelingprep.com"
+	IncomeStatementURL           = "api/v3/income-statement"
+	IncomeStatementURLFormat     = "api/v3/income-statement/%s"
+	QueryParamAPIKey             = "apikey"
+	QueryParamPeriod             = "period"
+	QueryParamPeriodValueAnnual  = "annual"
+	QueryParamPeriodValueQuarter = "quarter"
+	QueryParamLimit              = "limit"
+	QueryParamLimitExample       = uint(100)
+)
